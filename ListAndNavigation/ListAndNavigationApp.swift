b78e1c7755582a84be5a73a33f24ced521ae8ef0
didik <didik.maryono@pawoon.com>
2022-04-22 23:40:40 +0700

    Initial Commit

---
+++ ListAndNavigation/ListAndNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAndNavigationApp.swift
+//  ListAndNavigation
+//
+//  Created by Didik Maryono on 22/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ListAndNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
